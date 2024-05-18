33d50f330c928ada04b6f1b33f4d22b8a1378511
mysuikabar <mysuikabar@users.noreply.github.com>
2024-05-18 14:05:56 +0000

    deploy: 4229bd3100131946e0a9a6c52641c1582e4f0857

---
+++ compiled/flight_log/models/flight_log.sql
@@ -0,0 +1,47 @@
+with tmp_flight_log as (
+  select
+    *,
+    first_value(altitude) over (partition by timestamp_trunc(timestamp, minute) order by timestamp rows between unbounded preceding and unbounded following) as first_altitude,
+    last_value(altitude) over (partition by timestamp_trunc(timestamp, minute) order by timestamp rows between unbounded preceding and unbounded following) as last_altitude,
+    first_value(timestamp) over (partition by timestamp_trunc(timestamp, minute) order by timestamp rows between unbounded preceding and unbounded following) as first_timestamp,
+    last_value(timestamp) over (partition by timestamp_trunc(timestamp, minute) order by timestamp rows between unbounded preceding and unbounded following) as last_timestamp
+  from
+    `glider-dash`.`data_lake`.`flight_log`
+),
+
+tmp_flight_log_aggregated as (
+  select
+    min(flight_id) as flight_id,
+    min(pilot) as pilot,
+    min(glider_type) as glider_type,
+    min(glider_id) as glider_id,
+    timestamp_trunc(timestamp, minute) as timestamp,
+    avg(latitude) as latitude,
+    avg(longitude) as longitude,
+    avg(altitude) as altitude,
+    min(circling) as circling,
+    min(first_altitude) as first_altitude,
+    min(last_altitude) as last_altitude,
+    min(first_timestamp) as first_timestamp,
+    min(last_timestamp) as last_timestamp
+  from
+    tmp_flight_log
+  group by
+    timestamp
+)
+
+select
+  flight_id,
+  pilot,
+  glider_type,
+  glider_id,
+  timestamp,
+  latitude,
+  longitude,
+  altitude,
+  circling,
+  (last_altitude - first_altitude) / timestamp_diff(last_timestamp, first_timestamp, second) as climb_rate
+from
+  tmp_flight_log_aggregated
+where
+  timestamp_diff(last_timestamp, first_timestamp, second) > 0
---
+++ compiled/flight_log/models/flight_log_with_weather_info.sql
@@ -0,0 +1,23 @@
+select
+  f.flight_id,
+  f.pilot,
+  f.glider_type,
+  f.glider_id,
+  f.timestamp,
+  f.latitude,
+  f.longitude,
+  f.altitude,
+  f.circling,
+  f.climb_rate,
+  a.temperature,
+  a.wind_speed,
+  a.wind_direction,
+  a.daylight_hours,
+  a.humidity,
+  a.rainfall
+from
+  `glider-dash`.`data_warehouse`.`flight_log` as f
+left join
+  `glider-dash`.`data_lake`.`amedas` as a
+  on
+    timestamp_trunc(timestamp_sub(f.timestamp, interval mod(extract(minute from f.timestamp), 10) minute), minute) = a.timestamp
